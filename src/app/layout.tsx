1f999bd9108e8461fb162713d7c03503e90432ec
Christos Porios <christos.porios@gmail.com>
2024-08-20 19:04:11 +0300

    social meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,26 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Varnava Wildfire 2024",
   description: "A visualization of the Varnava Wildifire",
+  openGraph: {
+    images: [
+      {
+        url: "/summary.png",
+
+      }
+    ],
+    title: "Varnava Wildfire 2024",
+    description: "A visualization of the Varnava Wildifire",
+  },
+  twitter: {
+    card: "summary_large_image",
+    images: [
+      {
+        url: "/summary.png",
+      }
+    ],
+    title: "Varnava Wildfire 2024",
+    description: "A visualization of the Varnava Wildifire",
+  }
 };
 
 export default function RootLayout({
