04f014515c241be2518b376e27fc9e2d81f7c991
Christos Porios <christos.porios@gmail.com>
2024-08-20 18:53:29 +0300

    fix theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,8 +23,6 @@ export default function RootLayout({
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
         >
           {children}
         </ThemeProvider>
